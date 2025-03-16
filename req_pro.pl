06bb36cf35d8ab6074eca8298532d992a7c010f1
jcarpio <jose.carpio@gmail.com>
2025-03-16 21:14:43 +0100

    Create req_pro.pl

---
+++ req_pro.pl
@@ -0,0 +1,93 @@
+slots_per_week(40).
+slots_per_day(8).
+
+%########## CURSO 1 ################%
+
+class_subject_teacher_times('Inf1T3C1', mat_i, prof_mat_i, 2).
+class_subject_teacher_times('Inf1T2C1', mat_i, prof_mat_i, 2).
+class_subject_teacher_times('Inf1T1C1', mat_i, prof_mat_i, 2).
+class_subject_teacher_times('Inf1T1C1', fis, prof_fis, 2).
+class_subject_teacher_times('Inf1T2C1', fis, prof_fis, 2).
+class_subject_teacher_times('Inf1T3C1', fis, prof_fis, 2).
+class_subject_teacher_times('Inf1T3C2', tec_comp, prof_tec_comp, 2).
+class_subject_teacher_times('Inf1T1C2', tec_comp, prof_tec_comp, 2).
+class_subject_teacher_times('Inf1T2C2', tec_comp, prof_tec_comp, 2).
+class_subject_teacher_times('Inf1T4C2', tec_comp, prof_tec_comp, 2).
+class_subject_teacher_times('Inf1T3C1', fun_prog, prof_fun_prog, 2).
+class_subject_teacher_times('Inf1T1C1', fun_prog, prof_fun_prog, 2).
+class_subject_teacher_times('Inf1T2C1', fun_prog, prof_fun_prog, 2).
+class_subject_teacher_times('Inf1T3C1', adm_eco, prof_adm_eco, 2).
+class_subject_teacher_times('Inf1T1C1', adm_eco, prof_adm_eco, 2).
+class_subject_teacher_times('Inf1T2C1', adm_eco, prof_adm_eco, 2).
+class_subject_teacher_times('Inf1T4C2', mat_ii, prof_mat_ii, 2).
+class_subject_teacher_times('Inf1T2C2', mat_ii, prof_mat_ii, 2).
+class_subject_teacher_times('Inf1T1C2', mat_ii, prof_mat_ii, 2).
+class_subject_teacher_times('Inf1T3C2', mat_ii, prof_mat_ii, 2).
+class_subject_teacher_times('Inf1T2C2', fun_alg, prof_fun_alg, 2).
+class_subject_teacher_times('Inf1T3C2', fun_alg, prof_fun_alg, 2).
+class_subject_teacher_times('Inf1T1C2', fun_alg, prof_fun_alg, 2).
+class_subject_teacher_times('Inf1T4C2', fun_alg, prof_fun_alg, 2).
+class_subject_teacher_times('Inf1T1C1', fun_comp, prof_fun_comp, 2).
+class_subject_teacher_times('Inf1T3C1', fun_comp, prof_fun_comp, 2).
+class_subject_teacher_times('Inf1T2C1', fun_comp, prof_fun_comp, 2).
+class_subject_teacher_times('Inf1T1C2', est_dat_i, prof_est_dat_i, 2).
+class_subject_teacher_times('Inf1T3C2', est_dat_i, prof_est_dat_i, 2).
+class_subject_teacher_times('Inf1T4C2', est_dat_i, prof_est_dat_i, 2).
+class_subject_teacher_times('Inf1T2C2', est_dat_i, prof_est_dat_i, 2).
+class_subject_teacher_times('Inf1T3C2', mat_iii, prof_mat_iii, 2).
+class_subject_teacher_times('Inf1T1C2', mat_iii, prof_mat_iii, 2).
+class_subject_teacher_times('Inf1T2C2', mat_iii, prof_mat_iii, 2).
+class_subject_teacher_times('Inf1T4C2', mat_iii, prof_mat_iii, 2).
+
+%########## CURSO 2 ################%
+
+class_subject_teacher_times('Inf2T2C1', met_prog, prof_met_prog, 2).
+class_subject_teacher_times('Inf2T1C1', met_prog, prof_met_prog, 2).
+class_subject_teacher_times('Inf2T1C1', bdd, prof_bdd, 2).
+class_subject_teacher_times('Inf2T2C1', bdd, prof_bdd, 2).
+class_subject_teacher_times('Inf2T1C1', red_comp, prof_red_comp, 2).
+class_subject_teacher_times('Inf2T2C1', red_comp, prof_red_comp, 2).
+class_subject_teacher_times('Inf2T1C1', comp_est, prof_comp_est, 2).
+class_subject_teacher_times('Inf2T2C1', comp_est, prof_comp_est, 2).
+class_subject_teacher_times('Inf2T2C1', sis_op, prof_sis_op, 2).
+class_subject_teacher_times('Inf2T1C1', sis_op, prof_sis_op, 2).
+class_subject_teacher_times('Inf2T2C2', est_dat_ii, prof_est_dat_ii, 2).
+class_subject_teacher_times('Inf2T1C2', est_dat_ii, prof_est_dat_ii, 2).
+class_subject_teacher_times('Inf2T1C2', ing_soft, prof_ing_soft, 2).
+class_subject_teacher_times('Inf2T2C2', ing_soft, prof_ing_soft, 2).
+class_subject_teacher_times('Inf2T2C2', ia, prof_ia, 2).
+class_subject_teacher_times('Inf2T1C2', ia, prof_ia, 2).
+class_subject_teacher_times('Inf2T1C2', arq_comp, prof_arq_comp, 2).
+class_subject_teacher_times('Inf2T2C2', arq_comp, prof_arq_comp, 2).
+
+%########## CURSO 3 ################%
+
+class_subject_teacher_times('Inf3T1C1', alg_mod, prof_alg_mod, 2).
+class_subject_teacher_times('Inf3T1C1', dis_sis_inf, prof_dis_sis_inf, 2).
+class_subject_teacher_times('Inf3T1C1', inter_red, prof_inter_red, 2).
+class_subject_teacher_times('Inf3T1C1', prog_conc, prof_prog_conc, 2).
+class_subject_teacher_times('Inf3T1C1', elab_proj, prof_elab_proj, 2).
+
+%########## CURSO 4 ################%
+
+class_subject_teacher_times('Inf4T1C1', red_avanz, prof_red_avanz, 2).
+class_subject_teacher_times('Inf4T1C1', rob, prof_rob, 2).
+class_subject_teacher_times('Inf4T1C1', vis_comp, prof_vis_comp, 2).
+class_subject_teacher_times('Inf4T1C1', min_datos, prof_min_datos, 2).
+class_subject_teacher_times('Inf4T1C1', proc_hab, prof_proc_hab, 2).
+class_subject_teacher_times('Inf4T1C1', anim_3d, prof_anim_3d, 2).
+class_subject_teacher_times('Inf4T1C1', des_web, prof_des_web, 2).
+class_subject_teacher_times('Inf4T1C1', mot_busq, prof_mot_busq, 2).
+
+room_ingles(r1,'Inf1T1C1',mat_i,2).
+room_ingles(r1,'Inf1T1C1',fun_prog,2).
+room_ingles(r1,'Inf1T1C1',adm_eco,2).
+room_ingles(r1,'Inf1T1C2',mat_ii,2).
+room_ingles(r1,'Inf1T1C1',fun_comp,2).
+room_ingles(r1,'Inf1T1C2',mat_iii,2).
+room_ingles(r1,'Inf2T1C1',red_comp,2).
+room_ingles(r1,'Inf3T1C1',dis_sis_inf,2).
+room_ingles(r1,'Inf3T1C2',ing_soft,2).
+room_ingles(r1,'Inf3T1C2',rep_con,2).
+room_ingles(r1,'Inf4T1C1',ia,2).
+room_ingles(r1,'Inf4T1C2',mod_bio,2).
